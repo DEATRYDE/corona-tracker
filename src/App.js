4e9969e244b5c5a7dcef81396e8fe78a5d4868b1
Rahul Dogra <rahuldogra1998@gmail.com>
2020-08-04 00:59:12 +0530

    Added Sorted Table

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import {
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import "./App.css";
+import Table from "./Table";
+import { sortData } from "./util";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -32,10 +35,12 @@ function App() {
             name: country.country, //United States, United Kingdom
             value: country.countryInfo.iso2, // US , UK
           }));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
-
     getCountriesData();
   }, []);
 
@@ -102,7 +107,7 @@ function App() {
       <Card className="app_right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3> Worldwide new Cases</h3>
           {/* Graph */}
         </CardContent>
