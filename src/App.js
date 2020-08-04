36f59022b589582f982515be46dd3705502e8c65
Rahul Dogra <rahuldogra1998@gmail.com>
2020-08-05 00:58:24 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ import "./App.css";
 import Table from "./Table";
 import { sortData } from "./util";
 import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -59,6 +62,9 @@ function App() {
       .then((data) => {
         setCountry(countryCode);
         setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -102,7 +108,8 @@ function App() {
             total={countryInfo.deaths}
           />
         </div>
-        <Map />
+
+        <Map center={mapCenter} zoom={mapZoom} />
       </div>
 
       <Card className="app_right">
